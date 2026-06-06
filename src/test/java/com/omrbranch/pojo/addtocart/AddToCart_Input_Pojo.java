b80755b2911f1deb9564a0ee195869e65d8c26c5
hemanthx4 <hemanthx4@gmail.com>
2026-06-06 14:00:13 +0530

    three methods have been added by tester 2

--- src/test/java/com/omrbranch/pojo/addtocart/AddToCart_Input_Pojo.java
+++ src/test/java/com/omrbranch/pojo/addtocart/AddToCart_Input_Pojo.java
@@ -11,7 +11,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AddToCart_Input_Pojo {
 	public String product_id;
-    public String product_variation_id;
-    public String type;
+	public String product_variation_id;
+	public String type;
+
+	public void testing1() {
+		System.out.println("Hemanth");
+	}
+
+	public void testing2() {
+		System.out.println("sam");
+	}
+
+	public void testing3() {
+		System.out.println("karthi");
+	}
 
 }
